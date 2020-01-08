b0b713eb6c11d71324de601675715a6f1c3f7ccc
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-01-08 17:51:47 -0500

    Andrew's First Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with help from Andrew
+          Learn React with Andrew
         </a>
       </header>
       <body>
