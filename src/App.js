3e1c94f6c387b9980c6e4f77b09e002007ed34a9
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-01-12 15:27:58 -0500

    Welcome

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'rbx/index.css';
+import { Title, Button, Container } from 'rbx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Andrew
-        </a>
-        <button class='aki'>Akshay's Button</button>
-        <button class='aki'>Jacob's Button</button>
-      </header>
-      <body>
-        <div>
-          <p>Hello</p>
-        </div>
-      </body>
-    </div>
+    <Container>
+      <Title>Welcome, John!</Title>
+    </Container>
   );
 }
 
