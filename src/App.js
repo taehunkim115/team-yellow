8f8bf07b1a5028a8cee7d420e21594ba37c2e8a8
sadiejhood <sadiehood2020@u.northwestern.edu>
2020-01-13 16:07:09 -0600

    seperated firestore functions into helper file

--- src/App.js
+++ src/App.js
@@ -2,41 +2,41 @@ import React from 'react';
 import './App.css';
 import 'rbx/index.css';
 import { Title, Button, Container } from 'rbx';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/firestore';
+import FirebaseHelper from './Functions/FirebaseHelper';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBTMi1z22HLI8uiyOvLBOU_TkgSEwj18s4",
-  authDomain: "checkin-26602.firebaseapp.com",
-  databaseURL: "https://checkin-26602.firebaseio.com",
-  projectId: "checkin-26602",
-  storageBucket: "checkin-26602.appspot.com",
-  messagingSenderId: "929723304013",
-  appId: "1:929723304013:web:af2b97d82f2e6c06cfb404"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database(); //database().ref("test/test/");
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBTMi1z22HLI8uiyOvLBOU_TkgSEwj18s4",
+//   authDomain: "checkin-26602.firebaseapp.com",
+//   databaseURL: "https://checkin-26602.firebaseio.com",
+//   projectId: "checkin-26602",
+//   storageBucket: "checkin-26602.appspot.com",
+//   messagingSenderId: "929723304013",
+//   appId: "1:929723304013:web:af2b97d82f2e6c06cfb404"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.database(); //database().ref("test/test/");
 
-const user = "User1"
+// const user = "User1"
 
-const CheckIn = () => {
-  console.log('You checked in!');
-  var date = new Date();
-  db.collection("test").doc(user).set({ time:date });
-}
+// const CheckIn = () => {
+//   console.log('You checked in!');
+//   var date = new Date();
+//   db.collection("test").doc(user).set({ time:date });
+// }
 
 function App() {
+  console.log(FirebaseHelper)
+  console.log(FirebaseHelper.user)
   return (
     <Container>
       <Button.Group align="centered">
-        <Title>Welcome, { user }!</Title>
+        <Title>Welcome, { FirebaseHelper.user }!</Title>
       </Button.Group>
 
       <Button.Group align="centered">
-        <Button rounded={ true } color={ 'danger' } size={ 'large' } onClick={ CheckIn }>CheckIn</Button>
+        <Button rounded={ true } color={ 'danger' } size={ 'large' } onClick={ FirebaseHelper.CheckIn }>CheckIn</Button>
       </Button.Group>
     </Container>
   );
---
+++ src/Functions/FirebaseHelper.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/firestore';
+import React from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTMi1z22HLI8uiyOvLBOU_TkgSEwj18s4",
+    authDomain: "checkin-26602.firebaseapp.com",
+    databaseURL: "https://checkin-26602.firebaseio.com",
+    projectId: "checkin-26602",
+    storageBucket: "checkin-26602.appspot.com",
+    messagingSenderId: "929723304013",
+    appId: "1:929723304013:web:af2b97d82f2e6c06cfb404"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); //database().ref("test/test/");
+
+const user = "User1";
+
+const CheckIn = () => {
+    console.log('You checked in!');
+    var date = new Date();
+    db.collection("test").doc(user).set({ time:date });
+}
+
+const FirebaseHelper = {
+    user, 
+    CheckIn
+}
+
+export default FirebaseHelper;
