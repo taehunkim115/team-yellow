7cf26d1ad2f796befe7ef3e9ed36a324bfa58d3b
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-01-15 18:36:24 -0500

    Add Contact Not Working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'rbx/index.css';
-import { Title, Button, Container, Table } from 'rbx';
+import { Title, Button, Container, Table, Field, Control, Input } from 'rbx';
 import FirebaseHelper from './Functions/FirebaseHelper';
 
 const currentDate = new Date();
@@ -12,25 +12,12 @@ const ButtonEnabled = (time) => {
   );
 };
 
-const EmergencyContacts = (contacts) => {
-  console.log(contacts);
 
-  return (
-    <Table>
-        <Table.Head>Emergency Contact</Table.Head>
-        <Table.Body>
-          {contacts.map(contact =>
-            <Table.Row>
-              <Table.Cell>{contact.name}</Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
-  );
-};
 
 const App = () => {
   const [disabled, setDisabled] = useState(true);
+  const [contacts, setContacts] = useState([]);
+
   FirebaseHelper.FetchTime().then(time => { 
     setDisabled(ButtonEnabled(time));
   });
@@ -40,20 +27,64 @@ const App = () => {
     setDisabled(true);
   }
 
+  const AddContact = (name, number) => {
+    var newContacts = contacts;
+    newContacts.push({name:name, number:number});
+    setContacts(newContacts);
+  }
+
+  const EmergencyContacts = ({contacts}) => {
+    return (
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Heading>
+              Emergency Contacts
+            </Table.Heading>
+          </Table.Row>
+        </Table.Head>
+
+        <Table.Body>
+          {contacts.map(contact =>
+            <Table.Row>
+              <Table.Cell>
+                {contact.name}
+              </Table.Cell>
+              <Table.Cell>
+                X
+              </Table.Cell>
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
+    );
+  };
+
   return (
     <Container>
       <Button.Group align="centered">
         <Title>Welcome, { FirebaseHelper.user }!</Title>
       </Button.Group>
+
       <Button.Group align="centered">
         <Button rounded={ true } color={ 'danger' } size={ 'large' } onClick={ ButtonClick } disabled={ disabled }>CheckIn</Button>
       </Button.Group>
       <div className='checkin-text' hidden={ !disabled }>You CheckedIn!</div>
       <div className='checkin-text' hidden={ disabled }>Please CheckIn!</div>
-      <EmergencyContacts contacts={ [{name:'Jane Doe'}, {name: 'Jhon Doe'}] }></EmergencyContacts>
+
+      <EmergencyContacts contacts={ contacts }/>
+      <Field>
+        <Control>
+          <Input type='text' placeholder="Contact's Name"/>
+        </Control>
+        <Control>
+          <Input type='text' placeholder="Contact's Number"/>
+        </Control>
+      </Field>
       <Button.Group align='centered'>
-        <Button size={ 'medium' } color={ 'info' }>Edit Emergency Contacts</Button>
+        <Button size={ 'medium' } color={ 'info' } onClick={() => AddContact("test", "test")}>Add Emergency Contacts</Button>
       </Button.Group>
+
     </Container>
   )
 };
